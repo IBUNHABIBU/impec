e7d0fb3de3c163d915c27435a5a1ee7c134e98d6
IBUNHABIBU <ibunhabib@gmail.com>
2026-03-17 10:01:46 +0300

    Fix nav

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -32,7 +32,7 @@ module NavigationHelper
       ["Tours", travel_tours_path],
       ["Destinations", destinations_path],
       ["Trekking", trekkings_path],
-      ["Wildlife Safari", safaris_path],
+      # ["Wildlife Safari", safaris_path],
       ["Testimonials", testimonials_path],
       ["Contact Us", pages_contact_path]
     ]
